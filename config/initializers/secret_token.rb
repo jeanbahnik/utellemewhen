2283c1a6edd29b68ea91cbbf0e52fabee77597b5
Jean Bahnik <jeanbahnik@gmail.com>
2011-11-12 11:41:21 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tellmewhen::Application.config.secret_token = 'd215e45357d73e11058d1a97256c0c937fc23c2f93ac05d9086bbd9e2a16f77d730825e2d7b50a6dab2fd4f976bdf2ee3f764eddf235de0b46e6dd79f2347c7d'
