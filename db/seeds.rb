8c8c06162b8585fe68717db2dd7bccd1ca8b77c8
Luis Vega <luiscvega@gmail.com>
2012-01-20 10:36:10 -0600

    Changed seed file for e-mail for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,46 +6,23 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
-Race.create :name => "Prodigy Run", 
-            :city => "Chicago", 
-            :race_type => "5K", 
-            :race_on => "11/28/2011",
-            :size => "< 3000"
+Event.create :name => "Prodigy Run"
 puts "Created a race"
 
-Race.create :name => "Chicago's Perfect 10", 
-            :city => "Chicago", 
-            :race_type => "10 Miles", 
-            :race_on => "12/15/2011",
-            :size => "3000-10,000"
+Event.create :name => "Chicago's Perfect 10"
 puts "Created a race"
 
-Race.create :name => "Pilgrim Pacer 5-K", 
-            :city => "Chicago", 
-            :race_type => "Full Marathon", 
-            :race_on => "",
-            :size => "< 3000"
+Event.create :name => "Pilgrim Pacer 5-K"
 puts "Created a race"
 
-Race.create :name => "Chicago Marathon 2012", 
-            :city => "Chicago" 
+Event.create :name => "Chicago Marathon 2012" 
 puts "Created a race"
 
-Race.create :name => "Chicago Marathon 2013", 
-            :city => "Chicago" 
+Event.create :name => "Chicago Marathon 2013"
 puts "Created a race"
 
-Race.create :name => "Freedom Half Marathon", 
-            :city => "Chicago", 
-            :race_type => "Half Marathon", 
-            :race_on => "4/19/2012",
-            :size => ">10,000"
+Event.create :name => "Freedom Half Marathon"
 puts "Created a race"
 
-Race.create :name => "Wine Vine Run Half-Marathon & 5-K", 
-            :city => "Chicago", 
-            :race_type => "Half Marathon", 
-            :race_on => "5/12/2012",
-            :registration_on => "1/15/2012",
-            :size => "< 3000"
+Event.create :name => "Wine Vine Run Half-Marathon & 5-K"
 puts "Created a race"
