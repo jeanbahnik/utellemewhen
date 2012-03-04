91ee48c1ddc99bc36aa4243d627c0f37e3ba8470
Jean Bahnik <jeanbahnik@gmail.com>
2012-03-04 10:37:40 -0600

    Reaplaced sensitive data with ENV

--- app/controllers/race_names_controller.rb
+++
@@ -1,8 +0,0 @@
-class RaceNamesController < ApplicationController
-  
-  def index
-    @race_name = Event.order(:name).where("LOWER(name) like ?", "%#{params[:term].downcase}%")
-    render json: @race_name.map(&:name)
-  end
-
-end
--- app/helpers/race_names_helper.rb
+++
@@ -1,2 +0,0 @@
-module RaceNamesHelper
-end
